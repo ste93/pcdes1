3397ea570290ec516f30308984f17839ccfad7f5
Stefano Bernagozzi <stefano.bernagozzi@studio.unibo.it>
2019-05-30 10:38:47 +0200

    updated planet manager and worker

--- PlanetManager.java
+++ PlanetManager.java
@@ -12,16 +12,55 @@ public class PlanetManager {
 			planetsList.add(new Planet((int)(Math.random() * Constants.DRAWING_PANEL_SIZE_X),(int)(Math.random() * Constants.DRAWING_PANEL_SIZE_Y)));
 		}
 	}
-	
+
 	public ArrayList<Planet> getPlanetsList() {
 		return this.planetsList;
 	}
 
-	public void updatePlanet(int planetNumber) {
-		//planetsList[planetNumber].
+	public void updatePlanetsAcceleration(int planet1Number, int planet2Number, String axis) {
+    	int delta;
+    	Planet planetA = this.planetsList.get(planet1Number);
+    	Planet planetB = this.planetsList.get(planet2Number);
+    	if( axis == "x") {
+    		delta = planetA.getPositionX() - planetB.getPositionX();
+    	}else {
+    		delta = planetA.getPositionY() - planetB.getPositionY();
+    	} 
+    	double deltaDouble = delta * delta;
+        double accelerationA = (planetB.getMassPerGravity()) / (deltaDouble);
+        double accelerationB = (-1 * planetA.getMassPerGravity()) /(deltaDouble);
+
+        if( axis == "x") {
+    		planetA.updateAccelerationX(accelerationA);
+    		planetB.updateAccelerationX(accelerationB);
+    	}else {
+    		planetA.updateAccelerationY(accelerationA);
+    		planetB.updateAccelerationY(accelerationB);
+    	}
 	}
 	
-	public synchronized int getPairToCalculate() {
-		return 0;
-	}
+    private int calculateNewPosition(double initialPosition, double acceleration, double initialSpeed) {
+    	int newPosition = (int) (initialPosition + 
+    			0.5*acceleration * Constants.DELTA_TIME * Constants.DELTA_TIME +
+    			initialSpeed * Constants.DELTA_TIME
+    			);
+    			
+    	return newPosition;
+    }
+    
+    public void updatePlanetPosition(int planetNumber, String axis) {
+    	Planet planet = this.planetsList.get(planetNumber);
+    	if( axis == "x") {
+    		planet.setPositionX(
+    				calculateNewPosition(planet.getPositionX(),
+    						planet.getAccelerationX(),
+    						planet.getSpeedX()));
+    	}else {
+    		planet.setPositionY(
+    				calculateNewPosition(planet.getPositionY(),
+    						planet.getAccelerationY(),
+    						planet.getSpeedY())); 
+    	}     	
+    }
 }
+
--- Worker.java
+++ Worker.java
@@ -8,57 +8,43 @@ public class Worker {
 	}
 	
 	public void startWorker() {
-		
+		for(int i = 0; )
 	}
 	
 	
     private static class ComputeAccelerations implements Runnable {
-    	Planet planetA;
-    	Planet planetB;
+    	int planet1Number;
+    	int planet2Number;
+    	PlanetManager planetManager;
     	String axis;
 
-        public ComputeAccelerations(Planet planetA, Planet planetB, String axis) {
-            this.planetA = planetA;
-            this.planetB = planetB;
+        public ComputeAccelerations(PlanetManager planetManager, int planet1Number, int planet2Number, String axis) {
+        	this.planet1Number = planet1Number;
+        	this.planet2Number = planet2Number;
+        	this.planetManager = planetManager;
             this.axis = axis;
         }
 
         @Override
         public void run() {
-        	int delta;
-        	if( axis == "x") {
-        		delta = planetA.getPositionX() - planetB.getPositionX();
-        	}else {
-        		delta = planetA.getPositionY() - planetB.getPositionY();
-        	}
-            //double force = G * planetA.mass * planetB.mass / (Math.pow(distance, 2));
-            double accelerationA = (planetB.getMassPerGravity()) / delta;
-            double accelerationB = (-1 * planetA.getMassPerGravity()) / delta;
+        	planetManager.updatePlanetsAcceleration(planet1Number, planet2Number, axis);
+        }
+    }
+    
+    private static class ComputePositions implements Runnable {
+    	int planetNumber;
+    	PlanetManager planetManager; 
+    	String axis;
 
-            if( axis == "x") {
-        		planetA.updateAccelerationX(accelerationA);
-        		planetB.updateAccelerationX(accelerationB);
-        	}else {
-        		planetA.updateAccelerationY(accelerationA);
-        		planetB.updateAccelerationY(accelerationB);
-        	}
-            /*
-            
-            if(deltaY != 0) {
-                double accelerationAY = accelerationA * Math.sqrt(1 / (1 + Math.pow(deltaX / deltaY, 2)));
-                double accelerationAX = accelerationAY * Math.abs(deltaX / deltaY);
-                double accelerationBY = accelerationB * Math.sqrt(1 / (1 + Math.pow(deltaX / deltaY, 2)));
-                double accelerationBX = accelerationBY * Math.abs(deltaX / deltaY);
-                accelerations[a][b][0] = accelerationAX * (deltaX > 0 ? -1 : 1);
-                accelerations[a][b][1] = accelerationAY * (deltaY > 0 ? -1 : 1);
-                accelerations[b][a][0] = accelerationBX  * (deltaX < 0 ? -1 : 1);
-                accelerations[b][a][1] = accelerationBY  * (deltaY < 0 ? -1 : 1);
-            } else {
-                accelerations[a][b][0] = accelerationA  * (deltaX > 0 ? -1 : 1);
-                accelerations[a][b][1] = 0;
-                accelerations[b][a][0] = accelerationB  * (deltaX < 0 ? -1 : 1);
-                accelerations[b][a][1] = 0;
-            }*/
+        public ComputePositions(int planetNumber, String axis, PlanetManager planetManager) {
+            this.planetManager = planetManager;
+            this.planetNumber = planetNumber;
+            this.axis = axis;
+        }
+        
+        @Override
+        public void run() {
+        	planetManager.updatePlanetPosition(planetNumber, axis);
         }
     }
 }
